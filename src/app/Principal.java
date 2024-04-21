153cfbbecb7708a45c9c5762c94bf8d930bd73b4
Damian Aguilar <161623598+LucioDamian@users.noreply.github.com>
2024-04-21 17:30:00 +0200

    Correccion Final

--- src/app/Principal.java
+++ src/app/Principal.java
@@ -1,30 +1,39 @@
 package app;
 
+import java.util.Random;
+
 import util.Algoritmos;
 
-public class Principal {
+public class Principal extends Algoritmos{
 
-    public static int[] calcularFibonacci(int[] numeros) {
-        int[] resultadoFibonacci = new int[numeros.length];
-        for (int i = 0; i < numeros.length; i++) {
-            resultadoFibonacci[i] = Algoritmos.fibonacci(numeros[i]);
-        }
-        return resultadoFibonacci;
-    }
+	public static void main(String[] args) {
+        
+		
+	
+			 // Crear un array de 5 números aleatorios del 1 al 10
+	        int[] numeros = new int[5];
+	        Random random = new Random();
+	        for (int i = 0; i < numeros.length; i++) {
+	            numeros[i] = random.nextInt(10) + 1;
+	        }
 
-    public static int[] calcularFactorial(int[] numeros) {
-        int[] resultadoFactorial = new int[numeros.length];
-        for (int i = 0; i < numeros.length; i++) {
-            resultadoFactorial[i] = Algoritmos.factorial(numeros[i]);
-        }
-        return resultadoFactorial;
-    }
+	        // Calcular el número de Fibonacci de cada número y almacenar los resultados en un nuevo array
+	        int[] resultadoFibonacci = new int[numeros.length];
+	        for (int i = 0; i < numeros.length; i++) {
+	            resultadoFibonacci[i] = Algoritmos.fibonacci(numeros[i]);
+	        }
 
-    public static boolean[] verificarPrimos(int[] numeros) {
-        boolean[] resultadoPrimos = new boolean[numeros.length];
-        for (int i = 0; i < numeros.length; i++) {
-            resultadoPrimos[i] = Algoritmos.esPrimo(numeros[i]);
-        }
-        return resultadoPrimos;
+	        // Calcular el factorial de cada número y almacenar los resultados en un nuevo array
+	        int[] resultadoFactorial = new int[numeros.length];
+	        for (int i = 0; i < numeros.length; i++) {
+	            resultadoFactorial[i] = Algoritmos.factorial(numeros[i]);
+	        }
+
+	        // Comprobar si cada número es primo y almacenar los resultados en un nuevo array de booleanos
+	        boolean[] resultadoPrimos = new boolean[numeros.length];
+	        for (int i = 0; i < numeros.length; i++) {
+	            resultadoPrimos[i] = Algoritmos.esPrimo(numeros[i]);
+	        }
+
     }
 }
