0b0f35f24a0751b4ab504df8258bb32c09b0ea2e
Damian Aguilar <161623598+LucioDamian@users.noreply.github.com>
2024-04-21 16:41:07 +0200

    Ultima actualizacion

---
+++ src/app/PrincipalTest.java
@@ -0,0 +1,19 @@
+package app;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PrincipalTest {
+
+	@Test
+	final void testCalcularFactorial() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testVerificarPrimos() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
